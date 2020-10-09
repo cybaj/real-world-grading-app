5465de2554eaf9dca2e82520f3e530e0db01a796
cybaj <cybaj@naver.com>
2020-10-09 16:07:27 +0900

    Fix to MailGun

--- src/plugins/email.ts
+++ src/plugins/email.ts
@@ -1,7 +1,9 @@
 import Hapi from '@hapi/hapi'
 import Joi from '@hapi/joi'
 import Boom from '@hapi/boom'
-import sendgrid from '@sendgrid/mail'
+import { NodeMailgun } from 'ts-mailgun'
+
+const mailer = new NodeMailgun()
 
 // Module augmentation to add shared application state
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/33809#issuecomment-472103564
@@ -14,14 +16,20 @@ declare module '@hapi/hapi' {
 const emailPlugin = {
   name: 'app/email',
   register: async function (server: Hapi.Server) {
-    if (!process.env.SENDGRID_API_KEY) {
+    if (!process.env.MAILGUN_API_KEY) {
       server.log(
         'warn',
-        `The SENDGRID_API_KEY env var must be set, otherwise the API won't be able to send emails. Using debug mode which logs the email tokens instead.`,
+        `The MAILGUN_API_KEY env var must be set, otherwise the API won't be able to send emails. Using debug mode which logs the email tokens instead.`,
       )
       server.app.sendEmailToken = debugSendEmailToken
     } else {
-      sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+      mailer.apiKey = process.env.MAILGUN_API_KEY
+      mailer.domain = process.env.MAILGUN_DOMAIN ?? ''
+      mailer.fromEmail = "bowerco.tester@gmail.com"
+      mailer.fromTitle = "bowerco auth mail"
+      mailer.init()
+      console.log(mailer)
+
       server.app.sendEmailToken = sendEmailToken
     }
   },
@@ -32,12 +40,14 @@ export default emailPlugin
 async function sendEmailToken(email: string, token: string) {
   const msg = {
     to: email,
-    from: 'norman@prisma.io',
     subject: 'Login token for the modern backend API',
     text: `The login token for the API is: ${token}`,
   }
 
-  await sendgrid.send(msg)
+  await mailer.send(msg.to, msg.subject, msg.text)
+    .then((result) => console.log('Mail Sending Done', result))
+    .catch((error) => console.error('Mail Sending Error: ', error));
+
 }
 
 async function debugSendEmailToken(email: string, token: string) {
